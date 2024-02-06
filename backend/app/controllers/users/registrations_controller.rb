10e1d33cc647aaa256d3e505466de2788d9d1af1
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-02-06 16:49:32 +0000

    Add Auth-Basic

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -1,10 +1,20 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionFix
   respond_to :json
+  before_action :configure_sign_up_params, only: [:create]
 
   private
+
+  def configure_sign_up_params
+    if request.headers["Authorization"].present?
+      encoded_credentials = request.headers["Authorization"].split(" ").last
+      decoded_credentials = Base64.decode64(encoded_credentials)
+      email, password = decoded_credentials.split(":")
+      params[:user] = { email: email, password: password, password_confirmation: password }
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :other_required_fields])
+  end
 
   def respond_with(resource, _opts = {})
     if request.method == "POST" && resource.persisted?
@@ -23,6 +33,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 end
