c855734e7d44bd68bdcd45f6a964ad610fde4b2a
MiguelAngelHGProjects <miguelangelhernandezgarcia@alumno.ieselrincon.es>
2024-02-06 20:04:09 +0000

    main created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Authpage from './pages/Auth/authPage';
+import MainPage from './pages/Main/main.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Authpage />
+    <MainPage />
   </React.StrictMode>
 );
 reportWebVitals();
---
+++ frontend/src/pages/Main/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.css';
+
+function Main() {
+
+  return (
+    <>
+
+
+    </>
+  );
+}
+
+export default Main;
