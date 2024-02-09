b6568343db8b62d6b5a239a0e022c416f248ab9f
leogo22mz <leogo2206@gmail.com>
2024-02-09 19:27:31 +0000

    Solved merge problems

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -4,12 +4,12 @@ require "rails/all"
 
 # webpush declaration
  
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'webpush.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
+# config.before_configuration do
+#   env_file = File.join(Rails.root, 'config', 'webpush.yml')
+#   YAML.load(File.open(env_file)).each do |key, value|
+#     ENV[key.to_s] = value
+#   end if File.exists?(env_file)
+# end
 
 # Code snippet to show keys in console (anytime you run rails s)
 # require 'webpush'
