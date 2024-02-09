36c089b809e499b1bc14f769ca1245f14933c1d1
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-02-09 20:27:12 +0000

    Fiiiiix

--- backend/db/migrate/20240205181952_create_users.rb
+++ backend/db/migrate/20240205181952_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.boolean :is_admin, null: false
+      t.json :suscriptor
       # t.string :user_image, null: false, default: "default_user.png"
 
       t.timestamps
