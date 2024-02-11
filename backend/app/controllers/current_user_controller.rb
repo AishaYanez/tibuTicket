6b10362c90bea1c7ed497a843ec3c3f478e8f045
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-02-11 21:17:05 +0000

    Modify rak_session_fix.rb, add def destroy

--- backend/app/controllers/current_user_controller.rb
+++
@@ -1,7 +0,0 @@
-class CurrentUserController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    render json: UserSerializer.new(current_user).serializeable_hash[:data][:attributes], status: :ok
-  end
-end
