c1398bc2956afcbfd576284b037d785dfec7d839
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-02-18 00:11:17 +0000

    Add logout and delete account, error in btn menu in adminPage, isn't working

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ["DELETE", %r{^/logout$}],
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    jwt.expiration_time = 60.minutes.to_i
   end
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
