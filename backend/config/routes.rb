237b78a0bf206e3e9daf56a9de9638786204c1ff
MiguelAngelHGProjects <miguelangelhernandezgarcia@alumno.ieselrincon.es>
2024-02-20 18:30:09 +0000

    advanced jsreports

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :lists
-      # resources :users
+      namespace :users do
+        resources :users
+      end
     end
   end
 end
--- jsreports/data/TibuTicket/list-data/content.js
+++ jsreports/data/TibuTicket/list-data/content.js
@@ -20,11 +20,38 @@ async function fetchlists() {
     });
 }
 
+async function fetchusers() {
+    const options = {
+        hostname: 'localhost',
+        port: 4000,
+        path: '/api/v1/users/users',
+        method: 'GET',
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, (result) => {
+            let str = '';
+            result.on('data', (b) => str += b);
+            result.on('error', reject);
+            result.on('end', () => resolve(JSON.parse(str)));
+        });
+
+        req.end();
+    });
+}
+
 async function beforeRender(req, res) {
     try {
         req.data.lists = await fetchlists();
-        console.log(req.data.lists)
+        console.log(req.data.lists);
     } catch (error) {
         console.error('Error fetching lists:', error);
+    }
+
+    try {
+        req.data.users = await fetchusers();
+        console.log(req.data.users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
     }
 }
