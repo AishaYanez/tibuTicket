89aa92b9ae7e86a31921210a8da0e3e3578cafcc
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-02-20 23:04:42 +0000

    Modify ListCard and Main of ClientPage

--- frontend/src/components/ClientListCard/ListCard.js
+++ frontend/src/components/ClientListCard/ListCard.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import './ListCard.css';
 import Meat from '../../assets/images/meat.png';
 import { Button } from 'antd';
+import ListService from '../../services/ListService/list.service';
 
-function ListCard() {
+function ListCard({queue}) {
   const [buttonColor, setButtonColor] = useState('#D2D2D2');
   const [buttonTextColor, setButtonTextColor] = useState('#533D3D');
   const [displayText, setDisplayText] = useState(false);
@@ -14,17 +15,27 @@ function ListCard() {
     setDisplayText(true);
   };
 
+  const getTicket = (id) => {
+    handleButtonClick();
+
+    // ListService.getTicket(id).then((res) => {
+
+    // }).catch((err) => {
+      
+    // })
+  }
+
   return (
     <>
       <div className="ClienItemlistcomponent">
         <div className="Group1">
           <div className="ContentBlock" />
-          <img className="ListImg" src={Meat} alt=''/>
+          <img className="CardImg" src={queue.list_image ? queue.list_image.url : Meat} alt='Icono de la lista' />
         </div>
-        <div className="ListText">Charcutería</div>
-        <div className="Number">00</div>
+        <div className="ListText">{queue.list_description.list_name}</div>
+        <div className="Number">{queue.list_description.list_current_number}</div>
         <div className="ClientNumberText">Your number:</div>
-        <Button className="TicketButton" style={{ backgroundColor: buttonColor, color: buttonTextColor }} onClick={handleButtonClick}>
+        <Button onClick={() => getTicket(queue.list_description.id)} className="TicketButton" style={{ backgroundColor: buttonColor, color: buttonTextColor }}>
           <div className="TicketText">Ticket</div>
         </Button>
         {displayText && <div className='ClientNumber'>01</div>}
--- frontend/src/pages/Main-Client/main.js
+++ frontend/src/pages/Main-Client/main.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './main.css';
 import ListCard from '../../components/ClientListCard/ListCard';
+import ListService from '../../services/ListService/list.service';
 
 function Main() {
+  const [queues, setQueues] = useState([]);
+
+  async function fetchQueues() {
+    try {
+      const fetchedQueues = (await ListService.getLists()).data;
+      setQueues(fetchedQueues);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchQueues();
+  }, []);
+
 
   return (
     <>
       <div className="main-container">
         <div className='Usercardcontainer'>
+        {queues.map((q) => (
+            <ListCard key={q.list_description.id} queue={q} fetchQueues={fetchQueues} className="Items" />
+          ))}
+               {/* <ListCard className="Items" />
         <ListCard className="Items" />
         <ListCard className="Items" />
         <ListCard className="Items" />
-        <ListCard className="Items" />
-        <ListCard className="Items" />
+        <ListCard className="Items" /> */}
         </div>
       </div>
     </>
