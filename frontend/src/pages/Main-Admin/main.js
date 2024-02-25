95574844ca71e81be004fbc14d0f3c778ae8035e
leogo22mz <leogo2206@gmail.com>
2024-02-25 17:12:24 +0000

    Implemented Notification when logged-in at AdminPage

--- frontend/src/pages/Main-Admin/main.js
+++ frontend/src/pages/Main-Admin/main.js
@@ -7,26 +7,16 @@ import anime from 'animejs/lib/anime.es.js';
 import ListService from '../../services/ListService/list.service';
 import { useNavigate } from 'react-router-dom';
 
-function Main({queues}) {
+function Main({ queues }) {
   const nav = useNavigate();
   const [imageUploaded, setImageUploaded] = useState(false);
   const [imageFile, setImageFile] = useState();
   const [buttonPressed, setButtonPressed] = useState(false);
   const [newQueue, setNewQueue] = useState({ name: "" });
   const [formVisible, setFormVisible] = useState(false);
-  // const [queues, setQueues] = useState([]);
-
-  // async function fetchQueues() {
-  //   try {
-  //     const fetchedQueues = (await ListService.getLists()).data;
-  //     setQueues(fetchedQueues);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
 
   useEffect(() => {
-    
+
   }, []);
 
   const handleMouseDown = () => {
@@ -86,19 +76,126 @@ function Main({queues}) {
     localStorage.removeItem('user_image');
     localStorage.removeItem('lastLoginTime');
     nav('/');
-};
+  };
 
   const createQueue = (event) => {
     event.preventDefault();
     localStorage.getItem('token') ?
-    ListService.createList(formattedQueue()).then(r => {
-      message.success(r.data.status.message);
-    }).catch(err => {
-      const mess = err.response ? err.response.data.status.message : err.message;
-      message.error(mess);
-    }) : logoutActions();
+      ListService.createList(formattedQueue()).then(r => {
+        message.success(r.data.status.message);
+      }).catch(err => {
+        const mess = err.response ? err.response.data.status.message : err.message;
+        message.error(mess);
+      }) : logoutActions();
     setFormVisible(!formVisible)
   };
+
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // Establecer conexión WebSocket
+    const ws = new WebSocket('ws://localhost:4000/cable');
+    ws.onopen = () => {
+      console.log('WebSocket connected');
+      setSocket(ws);
+    };
+
+    // Registrarse en el Service Worker y suscribirse a las notificaciones VAPID
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.register('/serviceworker.js')
+        .then(function (reg) {
+          console.log('Service worker registered', reg);
+
+          fetch('http://localhost:4000/webpush/vapid_public_key')
+            .then(response => response.json())
+            .then(data => {
+              if (data.vapid_public_key) {
+                const base64String = data.vapid_public_key.replace(/-/g, '+').replace(/_/g, '/');
+                try {
+                  const vapidPublicKey = new Uint8Array(atob(base64String).split('').map(char => char.charCodeAt(0)));
+                  console.log('VAPID Public Key:', vapidPublicKey);
+
+                  // Suscribir al usuario cuando el componente se monta
+                  navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+                    subscribeUser(serviceWorkerRegistration, vapidPublicKey);
+                  });
+
+                } catch (error) {
+                  console.error('Error processing VAPID public key:', error);
+                }
+              } else {
+                console.error('VAPID public key not found in response');
+              }
+            })
+            .catch(error => console.error('Error fetching VAPID public key:', error));
+        })
+        .catch(function (err) {
+          console.error('Error during service worker registration:', err);
+        });
+    } else {
+      console.error('Service worker is not supported in this browser');
+    }
+  }, []);
+
+  function subscribeUser(serviceWorkerRegistration, vapidPublicKey) {
+    serviceWorkerRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: vapidPublicKey
+    })
+      .then(function (subscription) {
+        fetch('http://localhost:4000/notifications', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(subscription),
+        })
+          .then(r => {
+            if (!r.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return r.json();
+          })
+          .then(data => {
+            console.log('Subscription successful:', data);
+            // Enviar una notificación de prueba al suscribirse con éxito
+            sendTestNotification();
+          })
+          .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+          });
+      })
+      .catch((err) => {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Failed to subscribe the user:', err);
+        }
+      });
+  }
+
+  function sendTestNotification() {
+    fetch('http://localhost:4000/notifications/send_test_notification')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Test notification sent:', data);
+        // Enviar un comando a la consola de Rails si la notificación se envía correctamente
+        sendCommandToRailsConsole();
+      })
+      .catch(error => {
+        console.error('Error sending test notification:', error);
+      });
+  }
+
+  function sendCommandToRailsConsole() {
+    if (socket) {
+      const command = { type: 'send_command_to_rails_console', command: 'WebpushNotification.last.push("Session is logged in!")' };
+      socket.send(JSON.stringify(command));
+    } else {
+      console.error('WebSocket is not connected');
+    }
+  }
+
 
   return (
     <>
@@ -106,20 +203,20 @@ function Main({queues}) {
         <Menu />
         {formVisible &&
           <form onSubmit={createQueue} className='ListForm form'>
-          <Upload
-         name="avatar"
-         listType="picture-circle"
-         className="avatar-uploader"
-         maxCount={1}
-         onChange={handleChange}
-       >
-         {imageUploaded ? null : (
-           <img alt="" />
-         )}
-       </Upload>
-         <Input id='listName' onChange={changeData} value={newQueue.name} type="text" placeholder="Escribe aqui el nombre de la lista" required />
-         <Button type="primary" htmlType="submit" style={{ borderColor: 'black', backgroundColor: '#BBC0BA', color: 'white' }}>Enter</Button>
-       </form>
+            <Upload
+              name="avatar"
+              listType="picture-circle"
+              className="avatar-uploader"
+              maxCount={1}
+              onChange={handleChange}
+            >
+              {imageUploaded ? null : (
+                <img alt="" />
+              )}
+            </Upload>
+            <Input id='listName' onChange={changeData} value={newQueue.name} type="text" placeholder="Escribe aqui el nombre de la lista" required />
+            <Button type="primary" htmlType="submit" style={{ borderColor: 'black', backgroundColor: '#BBC0BA', color: 'white' }}>Enter</Button>
+          </form>
         }
 
         <div className='Admincardcontainer'>
